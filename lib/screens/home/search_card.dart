5e28b15c1d1a895c1c5e7c09cece37879b13f8a9
Shraya Mutsuddi <91559942+shrayamutsuddi@users.noreply.github.com>
2022-05-15 20:24:52 +0530

    SearrchCard Developed

--- lib/screens/home/search_card.dart
+++ lib/screens/home/search_card.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
 class SearchCard extends StatelessWidget {
+  const SearchCard({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(25),
+      margin: const EdgeInsets.all(25),
+      height: 300,
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+        children: const [
           Text('Search Here',
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.black,
                 fontSize: 26,
               ))
         ],
