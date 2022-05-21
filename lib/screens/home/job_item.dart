a8da24161714942ed2ee5ed43e8a47b1d9e3e03c
Shraya Mutsuddi <mutsuddishreya@gmail.com>
2022-05-22 00:11:34 +0530

    JobItem developed
    JobItem.dart developed. Code is running fine.

--- lib/screens/home/job_item.dart
+++ lib/screens/home/job_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_finder_app/reusable%20widget/icon_text.dart';
 import 'package:job_finder_app/screens/job.dart';
 
 class JobItem extends StatelessWidget {
@@ -14,7 +15,60 @@ class JobItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(30),
         color: Colors.white,
       ),
-      child: Text('Job Item'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.1),
+                    ),
+                    child: Image.asset(job.logoURL),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    job.company,
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              Icon(
+                  job.isMark ? Icons.bookmark : Icons.bookmark_outline_outlined,
+                  color: job.isMark
+                      ? Theme.of(context).primaryColor
+                      : Colors.black),
+            ],
+          ),
+          SizedBox(height: 15),
+          Text(
+            job.title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconText(Icons.location_on_outlined, job.location),
+              if (showTime) IconText(Icons.access_time_outlined, job.time),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
