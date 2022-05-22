0e013deeb4e74846fc45874b337635ed99ebb154
VishalSh-Dev <vishalshaw1658@gmail.com>
2022-05-22 17:31:27 +0530

    Added jobs and Recently added part

---
+++ lib/reusable widget/recent_job.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class RecentJobCard extends StatelessWidget {
+  final String companyName;
+  final String jobTitle;
+  final String logoImagePath;
+  final int hourlyRate;
+
+  RecentJobCard({
+    required this.companyName,
+    required this.jobTitle,
+    required this.logoImagePath,
+    required this.hourlyRate,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(15)),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Container(
+                  padding: EdgeInsets.all(9),
+                  height: 45,
+                  color: Color.fromARGB(192, 238, 238, 238),
+                  child: Image.asset(logoImagePath),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    jobTitle,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    companyName,
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Container(
+                  padding: EdgeInsets.all(5),
+                  color: Colors.green[300],
+                  child: Text(
+                    '\$' + hourlyRate.toString() + '/hr',
+                    style: TextStyle(color: Colors.white),
+                  ))),
+        ]),
+      ),
+    );
+  }
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,15 +3,20 @@ import 'package:job_finder_app/screens/home/job_list.dart';
 import 'package:job_finder_app/screens/home/search_card.dart';
 import 'package:job_finder_app/screens/home/tagList.dart';
 
+import '../../reusable widget/recent_job.dart';
 import 'home_app_bar.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final List recentJobs = [
+    ['Nike', 'Web Designer', 'assets/images/nike.png', 20],
+    ['Google', 'Product Dev', 'assets/images/google.png', 44],
+    ['Apple', 'Software Eng', 'assets/images/apple.png', 90],
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[90],
+      backgroundColor: Colors.grey[100],
       body: Stack(children: [
         Row(
           children: [
@@ -27,7 +32,38 @@ class HomePage extends StatelessWidget {
         ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [HomeAppBar(), SearchCard(), TagList(), JobList()],
+          children: [
+            HomeAppBar(),
+            SearchCard(),
+            TagList(),
+            JobList(),
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: Text(
+                'Recently Added',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: ListView.builder(
+                  itemCount: recentJobs.length,
+                  itemBuilder: (context, index) {
+                    return RecentJobCard(
+                      companyName: recentJobs[index][0],
+                      jobTitle: recentJobs[index][1],
+                      logoImagePath: recentJobs[index][2],
+                      hourlyRate: recentJobs[index][3],
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
         )
       ]),
     );
